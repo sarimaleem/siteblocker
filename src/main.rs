a0cc070b04c8970ff54bb8381bc52f9fbbe4583b
Sarim Aleem <sarima@Sarims-MacBook-Pro.local>
2023-07-25 23:04:25 -0700

    added sites from text file to etc hosts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use third_wheel
+use std::fs::{OpenOptions, self};
+use std::io::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let file_path = "./input.txt";
+    let input = fs::read_to_string(file_path).unwrap();
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("/etc/hosts")?;
+
+    writeln!(file, "### BLOCKLIST START")?;
+    input.lines().for_each(|s| {
+        println!("blocking      {}", s);
+        let _ = writeln!(file, "127.0.0.1       {}", s);
+    });
+    writeln!(file, "### BLOCKLIST END")?;
+
+    Ok(())
 }
